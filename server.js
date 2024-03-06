4cf720af79254df0504e072db11491e08a8840a7
jamestjy <97773112+jamestjy@users.noreply.github.com>
2024-03-06 21:47:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -221,12 +221,7 @@ async function checkAuthenticated(req, res, next) {
 }
 
 // Start the server
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
